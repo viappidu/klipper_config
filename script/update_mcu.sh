2f32bdf325ae1a6f9c5377605a8cd9afc0224571
Fabio Viappiani <viappidu@gmail.com>
2022-05-16 01:50:32 +0200

    works fine

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -7,35 +7,36 @@ cd ~/klipper
 echo "Start update mcu rpi"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
-make KCONFIG=/home/pi/klipper_config/script/config.linux_mcu
+# make menuconfig KCONFIG_CONFIG=~/klipper_config/script/config.linux_mcu
+# make KCONFIG=~/klipper_config/script/config.linux_mcu
 read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
+# make flash KCONFIG_CONFIG=~/klipper_config/script/config.linux_mcu
 echo "Finish update mcu rpi"
 echo ""
 
-# Update mcu XYE
-echo "Start update mcu XYE"
+# Update mcu Octopus
+echo "Start update mcu Octopus Pro F429"
 echo ""
-make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuXYE
-make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuXYE
-read -p "mcu XYE firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_lpc1769_1840010F871C4AAF863E7C5DC32000F5-if00 btt-skr-turbo-v1.4
-read -p "mcu XYE firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-echo "Finish update mcu XYE"
+#make clean
+#make menuconfig KCONFIG_CONFIG=~/klipper_config/script/config.octopus
+#make KCONFIG_CONFIG=~/klipper_config/script/config.octopus
+read -p "mcu Octopus firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+# ~/klipper/scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f429xx_3E001E000750314B33323220-if00 Octopus-Pro-F429
+read -p "mcu Octopus cannot be flashed, yet (missing support for SDIO). Copy ~/klipper/out/klipper.bin to firmware.bin on sd card manually. Press [Enter] to continue, or [Ctrl+C] to abort"
+echo "Finish update mcu Octopus"
 echo ""
 
-# Update mcu Z (!P1.29 setting)
-echo "Start update mcu Z"
+# Update mcu Ramps (!PH3, !PH6, !PL2, !PL4 setting)
+echo "Start update mcu Ramps"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuZ
-make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuZ
-read -p "mcu Z firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_lpc1769_07300110871C4AAFBF427C5DC72000F5-if00 btt-skr-turbo-v1.4
-read -p "mcu Z firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-echo "Finish update mcu Z"
+make menuconfig KCONFIG_CONFIG=~/klipper_config/script/config.ramps
+make KCONFIG_CONFIG=~/klipper_config/script/config.ramps
+read -p "mcu Ramps firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_55838323435351513021-if00
+#~/klipper/scripts/flash-usb.sh /dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_55838323435351513021-if00
+read -p "mcu Ramps firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+echo "Finish update mcu Ramps"
 echo ""
 
 sudo service klipper start
