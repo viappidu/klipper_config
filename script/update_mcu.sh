8642aeb8675d3bbd83fe306f99240c00e829fa41
Fabio Viappiani <viappidu@gmail.com>
2022-12-03 12:47:43 +0100

    Pre summer mods

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -4,27 +4,27 @@ sudo service klipper stop
 cd ~/klipper
 
 # Update mcu rpi
-echo "Start update mcu rpi"
-echo ""
-make clean
+# echo "Start update mcu rpi"
+# echo ""
+# make clean
 # make menuconfig KCONFIG_CONFIG=~/klipper_config/script/config.linux_mcu
 # make KCONFIG=~/klipper_config/script/config.linux_mcu
-read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+# read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 # make flash KCONFIG_CONFIG=~/klipper_config/script/config.linux_mcu
-echo "Finish update mcu rpi"
-echo ""
+# echo "Finish update mcu rpi"
+# echo ""
 
 # Update mcu Octopus
-echo "Start update mcu Octopus Pro F429"
-echo ""
-#make clean
-#make menuconfig KCONFIG_CONFIG=~/klipper_config/script/config.octopus
-#make KCONFIG_CONFIG=~/klipper_config/script/config.octopus
-read -p "mcu Octopus firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-# ~/klipper/scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f429xx_3E001E000750314B33323220-if00 Octopus-Pro-F429
-read -p "mcu Octopus cannot be flashed, yet (missing support for SDIO). Copy ~/klipper/out/klipper.bin to firmware.bin on sd card manually. Press [Enter] to continue, or [Ctrl+C] to abort"
-echo "Finish update mcu Octopus"
-echo ""
+# echo "Start update mcu Octopus Pro F429"
+# echo ""
+# #make clean
+# #make menuconfig KCONFIG_CONFIG=~/klipper_config/script/config.octopus
+# #make KCONFIG_CONFIG=~/klipper_config/script/config.octopus
+# read -p "mcu Octopus firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+# # ~/klipper/scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f429xx_3E001E000750314B33323220-if00 Octopus-Pro-F429
+# read -p "mcu Octopus cannot be flashed, yet (missing support for SDIO). Copy ~/klipper/out/klipper.bin to firmware.bin on sd card manually. Press [Enter] to continue, or [Ctrl+C] to abort"
+# echo "Finish update mcu Octopus"
+# echo ""
 
 # Update mcu Ramps (!PH3, !PH6, !PL2, !PL4 setting)
 echo "Start update mcu Ramps"
